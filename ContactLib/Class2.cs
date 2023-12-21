5bc7b45bdd9c7902419bb5d632fff3f6b7a9fcc6
Nurkhadis <nurkhadissandibek@gmail.com>
2023-12-21 16:13:22 +0600

    Добавьте файлы проекта.

---
+++ ContactLib/Class2.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactLib
+{
+    public class ContactManager
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(string name, string phoneNumber)
+        {
+            if (!IsValidPhoneNumber(phoneNumber))
+            {
+                Console.WriteLine("Ошибка: Введен некорректный номер телефона. Введите только цифры.");
+                return; // Прерываем выполнение метода при ошибке
+            }
+
+            Contact newContact = new Contact
+            {
+                Name = name,
+                PhoneNumber = phoneNumber
+            };
+            contacts.Add(newContact);
+            Console.WriteLine("Контакт добавлен.");
+        }
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return phoneNumber.All(char.IsDigit);
+        }
+        public void DisplayContacts()
+        {
+            Console.WriteLine("Список контактов:");
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Имя: {contact.Name}, Телефон: {contact.PhoneNumber}");
+            }
+        }
+
+        public Contact FindContact(string name)
+        {
+            return contacts.Find(contact => contact.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void RemoveContact(string name)
+        {
+            Contact contactToRemove = FindContact(name);
+            if (contactToRemove != null)
+            {
+                contacts.Remove(contactToRemove);
+                Console.WriteLine("Контакт удален.");
+            }
+            else
+            {
+                Console.WriteLine("Контакт не найден.");
+            }
+        }
+
+        public void UpdateContact(string name)
+        {
+
+            // Находим контакт по имени
+            Contact contactToUpdate = FindContact(name);
+
+            if (contactToUpdate != null)
+            {
+                Console.WriteLine($"Текущие данные контакта: Имя: {contactToUpdate.Name}, Телефон: {contactToUpdate.PhoneNumber}");
+
+                // Запрашиваем новые данные для обновления
+                Console.Write("Введите новое имя (оставьте пустым для сохранения текущего): ");
+                string newName = Console.ReadLine();
+
+                Console.Write("Введите новый номер телефона (оставьте пустым для сохранения текущего): ");
+                string newPhoneNumber = Console.ReadLine();
+
+                // Обновляем данные контакта, если пользователь ввел новые значения
+                if (!string.IsNullOrEmpty(newName))
+                {
+                    contactToUpdate.Name = newName;
+                }
+
+                if (!string.IsNullOrEmpty(newPhoneNumber))
+                {
+                    contactToUpdate.PhoneNumber = newPhoneNumber;
+                }
+
+                Console.WriteLine("Контакт успешно обновлен.");
+            }
+            else
+            {
+                Console.WriteLine("Контакт не найден.");
+            }
+        }
+
+        public void SaveContactsToFile(string fileName)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    foreach (var contact in contacts)
+                    {
+                        writer.WriteLine($"{contact.Name} {contact.PhoneNumber}");
+                    }
+                    Console.WriteLine("Контакты сохранены в файл.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении контактов в файл: {ex.Message}");
+            }
+        }
+
+        public void LoadContactsFromFile(string fileName)
+        {
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    using (StreamReader reader = new StreamReader(fileName))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(' ');
+                            if (parts.Length == 2)
+                            {
+                                Contact contact = new Contact
+                                {
+                                    Name = parts[0],
+                                    PhoneNumber = parts[1]
+                                };
+                                contacts.Add(contact);
+                            }
+                        }
+                        Console.WriteLine("Контакты загружены из файла.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Файл с контактами не найден.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при загрузке контактов из файла: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ContactManager/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactLib;
+
+namespace Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactManager contactManager = new ContactManager();
+
+            contactManager.LoadContactsFromFile("contacts.txt");
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить контакт");
+                Console.WriteLine("2. Показать контакты");
+                Console.WriteLine("3. Найти контакт");
+                Console.WriteLine("4. Изменить контакт");
+                Console.WriteLine("5. Удалить контакт");
+                Console.WriteLine("6. Выйти");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Введите имя: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Введите номер телефона: ");
+                        string phoneNumber = Console.ReadLine();
+                        contactManager.AddContact(name, phoneNumber);
+                        break;
+                    case "2":
+                        contactManager.DisplayContacts();
+                        break;
+                    case "3":
+                        Console.Write("Введите имя для поиска: ");
+                        string searchName = Console.ReadLine();
+                        Contact foundContact = contactManager.FindContact(searchName);
+                        if (foundContact != null)
+                        {
+                            Console.WriteLine($"Найден контакт: Имя: {foundContact.Name}, Телефон: {foundContact.PhoneNumber}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Контакт не найден.");
+                        }
+                        break;
+                    case "4":
+                        Console.Write("Введите имя для обновления: ");
+                        string updateName = Console.ReadLine();
+                        contactManager.UpdateContact(updateName);
+                        break;
+                    case "5":
+                        Console.Write("Введите имя для удаления: ");
+                        string deleteName = Console.ReadLine();
+                        contactManager.RemoveContact(deleteName);
+                        break;
+                    case "6":
+                        // Сохранение контактов в файл перед выходом
+                        contactManager.SaveContactsToFile("contacts.txt");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
